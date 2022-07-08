b02a9255981104e8e3aa7bdfe5c3cbbd9f171031
kim.taewooo <kim.taewooo@navercorp.com>
2022-07-09 02:46:27 +0900

    Fixed some errors

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,7 +4,6 @@ import { createRouter } from './context'
 import { postRouter } from './postRouter'
 import { userRouter } from './userRouter'
 
-export const appRouter = createRouter().transformer(superjson).merge('users.', userRouter).merge('posts.', postRouter)
+export const appRouter = createRouter().merge('users.', userRouter).merge('posts.', postRouter)
 
-// export type definition of API
 export type AppRouter = typeof appRouter
