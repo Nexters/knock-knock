8c0a718ec97e9d21aa165ee78b87396d4ba9cd96
kim-taewoo <acoustic0419@gmail.com>
2022-07-22 18:30:22 +0900

    Temp save mulfunctioning pointer event handlers

--- src/components/TimeSelectTable.tsx
+++ src/components/TimeSelectTable.tsx
@@ -1,18 +1,24 @@
-export default function TimeSelectTable() {
+interface Props {
+  isDragging: boolean
+  selectedIds: Set<string>
+  onPointerOver: (id: string) => void
+}
+
+export default function TimeSelectTable({ isDragging, selectedIds, onPointerOver }: Props) {
   return (
     <div className="flex w-full h-3/4 overflow-auto">
-      <div className="w-16 flex-shrink-0 flex-grow-0">
+      <div className="w-16 flex-shrink-0 flex-grow-0 select-none">
         <div className="h-6 flex-shrink-0 border-r first:border-t"></div>
         <div className="h-6 flex-shrink-0 border-r first:border-t"></div>
 
-        {[...Array(48)].map((_e, i2) => (
+        {[...Array(30)].map((_e, i2) => (
           <div key={i2} className="h-7 flex-shrink-0 border-r first:border-t last:border-b relative">
-            {i2 % 2 === 0 ? <span className="absolute left-5 -top-3">00시</span> : null}
+            {i2 % 2 === 0 ? <span className="absolute left-5 -top-3">{`${9 + i2 / 2}`.padStart(2, '0')}시</span> : null}
           </div>
         ))}
       </div>
 
-      <div className="flex-grow flex">
+      <div className="flex-grow flex" style={{ touchAction: 'none' }} onDragStart={() => false}>
         {[...Array(5)].map((_e, i) => (
           <div key={i} className="flex flex-col flex-shrink-0 w-1/4">
             <div>
@@ -20,11 +26,15 @@ export default function TimeSelectTable() {
               <div className="h-6 border-b border-r"></div>
             </div>
 
-            {[...Array(48)].map((_e, i2) => (
+            {[...Array(30)].map((_e, i2) => (
               <div
                 key={i2}
+                data-id={`${i}-${i2}`}
                 className="flex-shrink-0 h-7 border-b border-r even:border-dashed last-of-type:border-solid"
-                style={{ borderRight: '1px solid' }}></div>
+                style={{
+                  borderRight: '1px solid',
+                  backgroundColor: selectedIds.has(`${i}-${i2}`) ? 'green' : 'white',
+                }}></div>
             ))}
           </div>
         ))}
--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -1,10 +1,55 @@
+import { useEffect, useState } from 'react'
 import TimeSelectTable from '../../components/TimeSelectTable'
 
 export default function Event() {
+  const [isDragging, setIsDragging] = useState(false)
+  const [selectedCells, setSelectedCells] = useState(new Set<string>())
+
+  // console.log(selectedCells)
+
+  function addCell(cellId: string) {
+    setSelectedCells(prev => new Set([...prev, cellId]))
+  }
+
+  function removeCell(cellId: string) {
+    setSelectedCells(prev => new Set([...prev].filter(x => x !== cellId)))
+  }
+
+  function addOrRemoveCell(cellId: string) {
+    console.log(selectedCells)
+    console.log(selectedCells.has(cellId), cellId, 'check')
+    if (selectedCells.has(cellId)) removeCell(cellId)
+    else addCell(cellId)
+  }
+
+  useEffect(() => {
+    const handlePointerOver = e => {
+      console.log(e.target.dataset?.['id'])
+      addOrRemoveCell(e.target.dataset?.['id'])
+    }
+    const handlePointerCancel = () => console.log('canceled')
+    const handlePointerDown = e => {
+      addOrRemoveCell(e.target.dataset?.['id'])
+      document.addEventListener('pointerover', handlePointerOver)
+    }
+    const handlePointerUp = () => document.removeEventListener('pointerover', handlePointerOver)
+
+    document.addEventListener('pointerdown', handlePointerDown)
+    document.addEventListener('pointerup', handlePointerUp)
+    document.addEventListener('pointercancel', handlePointerCancel)
+
+    return () => {
+      document.removeEventListener('pointerdown', handlePointerDown)
+      document.removeEventListener('pointerup', handlePointerUp)
+      document.removeEventListener('pointerover', handlePointerOver)
+      document.removeEventListener('pointercancel', handlePointerCancel)
+    }
+  }, [])
+
   return (
     <div className="flex flex-col pt-9 h-screen relative">
       <div className="flex justify-between px-5">
-        <div className="text-sm">날짜 선택</div>
+        <div className="text-sm select-none">날짜 선택</div>
         <span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +64,7 @@ export default function Event() {
       </div>
 
       <div className="mt-7 h-full">
-        <TimeSelectTable />
+        <TimeSelectTable isDragging={isDragging} selectedIds={selectedCells} onPointerOver={addOrRemoveCell} />
       </div>
 
       <div className="absolute bottom-0 w-full flex justify-center mb-9">
