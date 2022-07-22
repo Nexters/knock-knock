642015286646c0b0e8b83a972e07fa03dcf08979
kim-taewoo <acoustic0419@gmail.com>
2022-07-22 16:26:57 +0900

    Added left side time indicators

--- src/components/TimeSelectTable.tsx
+++ src/components/TimeSelectTable.tsx
@@ -6,14 +6,16 @@ export default function TimeSelectTable() {
         <div className="h-6 flex-shrink-0 border-r first:border-t"></div>
 
         {[...Array(48)].map((_e, i2) => (
-          <div key={i2} className="h-7 flex-shrink-0 border-r first:border-t last:border-b"></div>
+          <div key={i2} className="h-7 flex-shrink-0 border-r first:border-t last:border-b relative">
+            {i2 % 2 === 0 ? <span className="absolute left-5 -top-3">00시</span> : null}
+          </div>
         ))}
       </div>
 
       <div className="flex-grow flex">
         {[...Array(5)].map((_e, i) => (
           <div key={i} className="flex flex-col flex-shrink-0 w-1/4">
-            <div className="bg-blue-300">
+            <div>
               <div className="h-6 border-t border-r"></div>
               <div className="h-6 border-b border-r"></div>
             </div>
