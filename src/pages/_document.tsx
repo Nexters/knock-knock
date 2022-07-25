84b1bc892511993d825d33dd6f52459b1392cd6c
kim-taewoo <acoustic0419@gmail.com>
2022-07-25 16:16:36 +0900

    Added theme color meta tag

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <head>
+          <meta name="theme-color" content="#16C674"></meta>
+        </head>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
