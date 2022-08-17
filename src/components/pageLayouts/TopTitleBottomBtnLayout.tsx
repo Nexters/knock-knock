8a4ee4e436715e2af95347de7ccb1bf4bdcfe073
kim-taewoo <acoustic0419@gmail.com>
2022-08-18 00:20:45 +0900

    Change BackDrop color and added more props for TopTitleBottomBtnLayout

--- src/components/pageLayouts/TopTitleBottomBtnLayout.tsx
+++ src/components/pageLayouts/TopTitleBottomBtnLayout.tsx
@@ -3,22 +3,32 @@ import TitleHeader from '../TitleHeader'
 
 interface Props {
   title: string
-  btnText: string
-  onBottomBtnClick: () => void
+  btnText?: string
+  onBottomBtnClick?: () => void
   onBackBtnClick?: () => void
+  customBtns?: ReactNode
   children: ReactNode
 }
 
-export default function TopTitleBottomBtnLayout({ title, btnText, onBackBtnClick, onBottomBtnClick, children }: Props) {
+export default function TopTitleBottomBtnLayout({
+  title,
+  btnText,
+  onBackBtnClick,
+  onBottomBtnClick,
+  customBtns,
+  children,
+}: Props) {
   return (
     <div className="flex flex-col items-center bg-bgColor h-screen pt-24 px-5 pb-8 overflow-auto relative">
       <TitleHeader title={title} onBackBtnClick={onBackBtnClick} />
       {children}
 
       <div className="mt-11 w-full">
-        <button className="btn btn-primary w-full" onClick={onBottomBtnClick}>
-          {btnText}
-        </button>
+        {customBtns ?? (
+          <button className="btn btn-primary w-full" onClick={onBottomBtnClick}>
+            {btnText}
+          </button>
+        )}
       </div>
     </div>
   )
