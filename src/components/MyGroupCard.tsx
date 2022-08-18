68a3bcc52d4119e03ae14294d251050942024011
yojuyoon <ziyoung1121@gmail.com>
2022-08-18 21:12:37 +0900

    fix: calendar background hover ui

--- src/components/MyGroupCard.tsx
+++ src/components/MyGroupCard.tsx
@@ -12,7 +12,7 @@ const MyGroupCard = ({ data }: MyGroupProps) => {
       <div className="flex items-center justify-between bg-cardBg p-3 rounded-lg mt-2 w-[100%]">
         <div className="flex items-center">
           <div className="font-bold text-base text-textLightGray">{data.name}</div>
-          <div className="ml-2 text-xs text-textGray2">참여 {data.members.length}명</div>
+          <div className="ml-2 text-xs text-textGray2">참여 {data.members?.length}명</div>
         </div>
         <img src="/assets/svg/right.svg" alt="logo" />
       </div>
